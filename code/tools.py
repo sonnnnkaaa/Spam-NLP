c354f2b61488877ce2f03a0000fb46dc0bbdf253
sofia <sofya.vedenina2005@gmail.com>
2026-04-08 20:18:42 +0300

    comments added

--- code/tools.py
+++ code/tools.py
@@ -8,43 +8,77 @@ from keras import layers, Sequential
 import tensorflow as tf
 
 
-MAX_WORDS = 10000
-MAX_LEN = 50
-RANDOM_STATE = 42
-TEST_SIZE = 0.2
-BATCH_SIZE = 32
-CV_EPOCHS = 10
-EPOCHS = 20
-N_CLASSES = 2
+MAX_WORDS = 10000 # Maximum number of words for the tokenizer
+MAX_LEN = 50      # Maximum sequence length
+RANDOM_STATE = 42 # Seed
+TEST_SIZE = 0.2   # 20% for test
+BATCH_SIZE = 32   # Batch size
+CV_EPOCHS = 10    # Number of epochs for cross-valiation scores
+EPOCHS = 20       # Number of epochs for the main model
+N_CLASSES = 2     # Number of classes for classification
 
 
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-tf.random.set_seed(RANDOM_STATE)
+tf.random.set_seed(RANDOM_STATE) # Setting seed
 
 
 def text_preprocessing(text):
-    text = text.lower()                   # lower register
-    text = re.sub("[^a-zA-Z]", " ", text) # cleaning
+    """
+      Lowers the case of text and removes unnecessary characters.
+
+      Args:
+          text (string): the string that needs to be processed
+
+      Returns:
+          string: processed string
+    """
+    text = text.lower()                   # Lower register
+    text = re.sub("[^a-zA-Z]", " ", text) # Cleaning
     return text
 
 
 def tokenizing(data, text_col, tar_col=None, max_words=MAX_WORDS, max_len=MAX_LEN, tokenizer_path=None):
-    data[text_col] = data[text_col].apply(text_preprocessing)
+    """
+      Tokenizes text and breaks it into sequences.
+
+      Args:
+          data (pandas DataFrame): data containing text
+          text_col (string): name of column containing text
+          tar_col (string): name of column containing labels (optional, default=None)
+          max_words (int): number of words for the tokenizer (default=MAX_WORDS)
+          max_len (int): sequences length
+          tokenizer_path (string): path to the tokenizer if this already exists (optional, default=None)
+
+      Returns:
+          ndarray: array with shape (, max_len)
+          ndarray: array with shape (,) or None if tar_col is not specified
+    """
+    data[text_col] = data[text_col].apply(text_preprocessing)                       # Applying text_preprocessing for text column of data
     if tokenizer_path:
-        tokenizer = joblib.load(tokenizer_path)
+        # If tokenizer already exists
+        tokenizer = joblib.load(tokenizer_path)                                     # Loading tokenizer
     else:
-        tokenizer = Tokenizer(num_words=MAX_WORDS, oov_token="<OOV>") # tokenizer
-        tokenizer.fit_on_texts(data[text_col])                 # fitting tokenizer
-        joblib.dump(tokenizer, "../models/tokenizer.pk1")
-    sequences = tokenizer.texts_to_sequences(data[text_col])                # sequences
-    X = pad_sequences(sequences, maxlen=MAX_LEN, padding="post", truncating="post")
+        tokenizer = Tokenizer(num_words=MAX_WORDS, oov_token="<OOV>")               # Creating tokenizer with OOV token
+        tokenizer.fit_on_texts(data[text_col])                                      # Fitting tokenizer on text data
+        joblib.dump(tokenizer, "../models/tokenizer.pk1")                           # Saving tokenizer
+    sequences = tokenizer.texts_to_sequences(data[text_col])                        # Breaking text into sequences
+    X = pad_sequences(sequences, maxlen=MAX_LEN, padding="post", truncating="post") # Reducing sequences to the same length
     if tar_col:
-        y = data[tar_col].values
+        # If tar_col specified
+        y = data[tar_col].values                                                    # Labels
         return X, y
     return X, None
 
 
 def create_model():
+    """
+      Creates and compiles NLP-model.
+
+      Returns:
+          Sequential: compiled Keras model
+    """
+
+    # Architechture of NLP-model
     nlp_model = Sequential([
         layers.Input(shape=(MAX_LEN,)),
         layers.Embedding(input_dim=MAX_WORDS, output_dim=128),
@@ -54,6 +88,8 @@ def create_model():
         layers.Dense(16, activation="relu"),
         layers.Dense(1, activation="sigmoid")
     ])
+
+    # Compiling model
     nlp_model.compile(optimizer="adam",
                   loss="binary_crossentropy",
                   metrics=["accuracy"])
--- predict.py
+++ predict.py
@@ -7,32 +7,48 @@ sys.path.append("code")
 import tools as t
 
 
-MODEL_PATH = "./models/spam_detector_nlp.keras"
-TOKENIZER_PATH = "./models/tokenizer.pk1"
+MODEL_PATH = "./models/spam_detector_nlp.keras" # Path to the model
+TOKENIZER_PATH = "./models/tokenizer.pk1"       # Path to the tokenizer
 
 
 def read_files():
+    """
+      Reads files for prediction.
+
+      Returns:
+          list: list of pandas DataFrames
+          list: names of files
+    """
     data = []
     names = []
-    dir_entries = os.listdir(".")
-    data_files = [f for f in dir_entries if f.endswith(".csv")]
+    dir_entries = os.listdir(".")                                                      # List of files in current directory
+    data_files = [f for f in dir_entries if f.endswith(".csv")]                        # List of .csv files in current directory
     for f in data_files:
         try:
-            data.append(pd.read_csv(f, header=None).iloc[:, [0]])
+            data.append(pd.read_csv(f, header=None).iloc[:, [0]])                      # Reading .csv file and adding it to data_files
         except Exception as e:
             print(f"An error occured while reading file {f}: {type(e).__name__}: {e}")
         else: 
             print(f"Found {f}")
 
-            names.append(f)
+            names.append(f)                                                            # Adding file name to names
     return data, names
         
 
 def predict(data):
-    nlp_model = load_model(MODEL_PATH)
-    X, _ = t.tokenizing(data.copy(), text_col=data.columns[0], tokenizer_path=TOKENIZER_PATH)
-    prob = nlp_model.predict(X, batch_size=1).reshape(-1)
-    prediction = (nlp_model.predict(X, batch_size=1) > 0.5).astype(int).reshape(-1)
+    """
+      Predicts labels.
+
+      Args:
+          data (pandas DataFrame): data for prediction
+
+      Returns:
+          pandas DataFrame: prediction result
+    """
+    nlp_model = load_model(MODEL_PATH)                                                        # Loading model
+    X, _ = t.tokenizing(data.copy(), text_col=data.columns[0], tokenizer_path=TOKENIZER_PATH) # Tokenizing data
+    prob = nlp_model.predict(X, batch_size=1).reshape(-1)                                     # Predicting probabilities
+    prediction = (nlp_model.predict(X, batch_size=1) > 0.5).astype(int).reshape(-1)           # Predicting labels
     result = pd.DataFrame({"data": data.iloc[:, 0],
                            "prediction": prediction,
                            "probability": prob})
@@ -40,6 +56,13 @@ def predict(data):
     
 
 def write_prediction(prediction, name):
+    """
+      Writes prediction into .csv
+
+      Args:
+          prediction (pandas DataFrame): prediction
+          name (string): name of file for prediction
+    """
     prediction.to_csv(f"prediction_{name}", index=False)
     
     
